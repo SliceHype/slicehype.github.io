dec26a255524fecb865383fdd203c7ff9ca9d9ca
Slice Hype <213822177+SliceHype@users.noreply.github.com>
2025-05-31 02:14:54 -0700

    js

--- assets/js/vid-data.js
+++ assets/js/vid-data.js
@@ -1,19 +1,2 @@
-async function fetchVideoData() {
-  try {
-    const response = await fetch("https://www.slicehype.tv/assets/json/config.json");
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching video data:", error);
-    return null;
-  }
-}
-
-async function fetchAll() {
-	return await fetchVideoData();
-}
-
-console.log(fetchAll());
+fetch("https://www.slicehype.tv/assets/json/config.json")
+  .then(res => console.log(res.json()));
