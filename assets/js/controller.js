91d236bcd159532f002ef7c469f8b75d6f785996
Slice Hype <213822177+SliceHype@users.noreply.github.com>
2025-05-31 03:17:07 -0700

    js

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,6 +1,9 @@
 async function fetchData() {
   try {
-    return await fetch("https://www.slicehype.tv/assets/json/config.json");
+    const res = await fetch("https://www.slicehype.tv/assets/json/config.json");
+    const data = await res.json();
+
+    return data;
   } catch (e) {
     console.error(e);
   }
