512279216290c963448c8ded12949f8a62b17fc8
Slice Hype <213822177+SliceHype@users.noreply.github.com>
2025-05-31 03:25:24 -0700

    Update controller.js

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,8 +1,9 @@
 async function fetchData() {
   try {
     const res = await fetch("https://www.slicehype.tv/assets/json/config.json");
+    const data = await res.json();
 
-    return res;
+    return data;
   } catch (e) {
     console.error(e);
   }
