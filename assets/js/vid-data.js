45537b4bd9ae1b244bcef6d072d8816d9c852fe6
Slice Hype <213822177+SliceHype@users.noreply.github.com>
2025-05-31 02:24:50 -0700

    Update vid-data.js

--- assets/js/vid-data.js
+++ assets/js/vid-data.js
@@ -1,2 +1,11 @@
-const data = fetch("https://www.slicehype.tv/assets/json/config.json")
-  .then((res) => { return res.json() });
+async function fetchData() {
+  try {
+    const res = await fetch('https://www.slicehype.tv/assets/json/config.json');
+    const data = await res.json();
+    console.log(data.categories);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+fetchData();
