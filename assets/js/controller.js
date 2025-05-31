00af85169d5e199e82a8a98545b1eacf30fb5204
Slice Hype <213822177+SliceHype@users.noreply.github.com>
2025-05-31 03:43:57 -0700

    footer test

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,3 +1,14 @@
+async function fetchFooter() {
+  try {
+    const res = await fetch("https://www.slicehype.tv/layout/footer.html");
+    const data = await res.text();
+
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 async function fetchData() {
   try {
     const res = await fetch("https://www.slicehype.tv/assets/json/config.json");
